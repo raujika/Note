e8fe40a11ce0c38c1f05126954d8584560129fb0
raujika <clvbcs@gmail.com>
2014-06-02 23:27:02 +0800

    Haskellref shortcut&change

--- Haskell/ref.hs
+++ Haskell/ref.hs
@@ -26,6 +26,57 @@ quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
 qsort [] = []
 qsort (p:xs) = (qsort [x | x <- xs, x < p]) ++ [p] ++ (qsort [x | x <- xs, x >= p])
 
+import System.Environment(getArgs)
+
+main :: IO()
+main = do
+       args <- getArgs
+       print args
+
+data Tr x = Leaf x | Branch (Tr x) (Tr x)
+data MF = [Char]
+
+class MF f where
+  kmap                  :: (a -> b) -> f a -> f b
+
+instance MF Tr where
+  kmap f (Leaf x)       = Leaf   (f x)
+  kmap f (Branch t1 t2) = Branch (kmap f t1) (kmap f t2)
+
+-- (\x -> x++"f") . (\x -> x++"s") $ "x"
+-- "xsf"
+
+
+--the "DO"
+--do x1 <- action1
+--   x2 <- action2
+--   action3 x1 x2
+--
+--action1 >>= \ x1 ->
+--action2 >>= \ x2 ->
+--action3 x1 x2
+--
+--action1
+--  >>=
+--    \
+--      x1
+--        ->
+--          action2
+--            >>=
+--              \
+--                x2
+--                  ->
+--                    action3
+--                      x1
+--                      x2
+
+--(simpleHttp "http://lianchun.myweb.hinet.net/Birthday_password/") >>= \ c -> return (L.unpack c)
+
+--(simpleHttp "http://lianchun.myweb.hinet.net/Birthday_password/") >>= \ c -> return (indexOf (L.unpack c) "<div align=\"center\"><a href=")
+
+--(simpleHttp "http://lianchun.myweb.hinet.net/Birthday_password/") >>= \ c -> return (map (28+) (indexOf (L.unpack c) "<div align=\"center\"><a href="))
+
+--(simpleHttp "http://lianchun.myweb.hinet.net/Birthday_password/") >>= \ c -> return (map (28+) (indexOf (L.unpack c) "<div align=\"center\"><a href="))
 
 
 
